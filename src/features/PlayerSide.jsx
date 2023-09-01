6e4d2e8dcb7b7acb5a7d49956875e71a2e453315
Mehran <mehranasadition@gmail.com>
2023-09-01 11:15:11 +0430

    src is completed

---
+++ src/features/PlayerSide.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function PlayerSide() {
+  return (
+    <div>PlayerSide</div>
+  )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,15 +4,16 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      colors:{
-
+      colors: {
+        darkest: "#343a40",
+        dark: "#495057",
+        medium: "#ced4da",
+        light: "#f1f3f5",
+        cyan: "#1098ad",
+        orange: "#ffa94d",
       },
-      screens:{
-
-      },
-      fontFamily:{
-        
-      }
+      screens: {},
+      fontFamily: {},
     },
   },
   plugins: [],
